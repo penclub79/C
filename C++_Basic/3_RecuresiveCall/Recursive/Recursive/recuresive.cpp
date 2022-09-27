a41a01f5c16cb0958cb18042d5fd78ec0b96f085
ShinHoCheol <penclub79@naver.com>
2022-09-27 17:39:53 +0900

    RecuresiveCall Example Code

---
+++ C++_Basic/3_RecuresiveCall/Recursive/Recursive/recuresive.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int recuresive(int iNum)
+{
+	// for문 처럼 조건을 주어야한다. (핵심)
+
+	if (0 >= iNum) // 재귀호출 멈추는 조건
+		return 0;
+	
+	printf("%d\n", iNum);
+
+	return recuresive(iNum - 1);
+}
+
+int main(void)
+{
+	recuresive(5);
+	return 0;
+}
